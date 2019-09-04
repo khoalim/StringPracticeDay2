e02a6947255c465345437f746527994b36c4f0a8
Kim Hoalim <kthoalim@gmail.com>
2019-09-04 15:42:31 -0400

    Add Last Name and Length Variable

--- StringPracticeDay2/Program.cs
+++ StringPracticeDay2/Program.cs
@@ -5,18 +5,40 @@ namespace StringPracticeDay2
     class Program
     {
         static void Main(string[] args)
-        {
-          
+        {         
 
-           Console.WriteLine("What is your first name?");
+            Console.WriteLine("What is your first name?");
             
-          string firstName;
-          firstName = Console.ReadLine(); // This is a METHOD it has a () with no parameters defined = is an asnment op
+            string firstName;
+            firstName = Console.ReadLine(); // This is a METHOD it has a () with no parameters defined = is an assignment operator
 
             Console.WriteLine("Your name is: " + firstName);
 
-        
+            Console.WriteLine("What is your last name?");
+
+            string lastName;
+            lastName = Console.ReadLine();
+
             
+            Console.WriteLine("Your name is: " + firstName.ToLower() + " " + lastName.ToLower()); // .ToLower to change case
+
+            Console.WriteLine("Which name is longer?");
+
+            int firstNameLength = firstName.Length;
+
+            int lastNameLength = lastName.Length;
+
+            Console.WriteLine("First Name has " + firstNameLength + " characters");
+
+            Console.WriteLine("Last Name has " + lastNameLength + " characters");
+
+
+
+
+                        
+
+            
+
         }
     }
 }
